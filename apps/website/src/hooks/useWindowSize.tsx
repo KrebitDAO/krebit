d0c38bcb1a8099f9ed00121ae33577feabcfa562
Andrés Montoya <andresmontoyafcb@gmail.com>
2022-07-05 15:48:00 -0500

    Feat: update order of code and add window size hook for small phones (#8)

---
+++ apps/website/src/hooks/useWindowSize.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    if (!window) return;
+
+    const handleResize = () => {
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+};
