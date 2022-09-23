cc8a8e73e37f41c3be50abcc4fd19ba4bb2f63ad
Alejandro Rios <arios@livevox.com>
2022-09-22 20:31:31 -0500

    re included my changes

--- apps/dapp/src/utils/mergeArray.ts
+++ apps/dapp/src/utils/mergeArray.ts
@@ -4,5 +4,7 @@ export const mergeArray = (arr: any[]): any[] => {
   for (const num of arr) {
     counts[num] = counts[num] ? counts[num] + 1 : 1;
   }
-  return Object.entries(counts).sort((a, b) => b[1] - a[1]);
+  return Object.entries(counts).sort(
+    (a, b) => (b[1] as number) - (a[1] as number)
+  );
 };
