363e6548be4e60aa8ba1cb5848821c6452a5d891
Alejandro Rios <arios@livevox.com>
2023-01-29 11:51:49 -0500

    initial Deals logic

--- apps/dapp/src/utils/checkCredentialsURLs.ts
+++ apps/dapp/src/utils/checkCredentialsURLs.ts
@@ -65,6 +65,5 @@ export const checkCredentialsURLs = (
         : `https://testnets.opensea.io/assets/mumbai/${contract}/${value}`;
   }
 
-  console.log('currentUrl:', currentUrl);
   window.open(currentUrl, '_blank');
 };
